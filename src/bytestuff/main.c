abc8aa17c5942f643db2336dedaa2c50014f3603
LucasLonegro <llonegrogurfinkel@itba.edu.ar>
2024-11-23 02:39:07 -0300

    fix bytestuff
    does it stuff bytes? does it do stuff relating to bytes? is it stuff made of bytes?
    
    we'll never know.

--- src/bytestuff/main.c
+++ src/bytestuff/main.c
@@ -3,40 +3,52 @@
 #include <stdio.h>
 #include <string.h>
 
+#define BLOCK 4
+
 /**
  * @brief Parse a file and make each line POP3 compliant
  */
 int main()
 {
-    char buffer[512 + 1];
-    while (fgets(buffer, sizeof(buffer), stdin))
+    char s[BLOCK];
+    int read_is_after_newline = 1, read_is_after_carriage_return = 0, last_print, i;
+    while (fgets(s, BLOCK, stdin) != NULL)
     {
-        const char *fmt = "%.510s\r\n";
-
-        size_t l = strlen(buffer);
-
-        bool starts_with_dot = buffer[0] == '.';
-        size_t max_length = starts_with_dot ? 509 : 510;
-        bool ends_with_crlf = l > 2 ? buffer[l - 2] == '\r' && buffer[l - 1] == '\n' : false;
-
-        if (starts_with_dot)
+        last_print = 0;
+        if (s[0] == '\n' && !read_is_after_carriage_return)
+            putchar('\r');
+        else if (s[0] == '.' && read_is_after_newline)
+            putchar('.');
+        for (i = 1; s[i]; i++)
         {
-            // Prepend a .
-            fmt = ".%.509s\r\n";
+            if (s[i] == '\n' && s[i - 1] != '\r')
+            {
+                fwrite(s + last_print, sizeof(char), i - last_print, stdout);
+                putchar('\r');
+                last_print = i;
+            }
+            else if (s[i] == '.' && s[i - 1] == '\n')
+            {
+                fwrite(s + last_print, sizeof(char), i - last_print + 1, stdout);
+                putchar('.');
+                last_print = i;
+            }
         }
-
-        if (ends_with_crlf)
+        if (i > last_print)
+            fwrite(s + last_print, sizeof(char), i - last_print, stdout);
+        if (i > 0)
         {
-            buffer[l - 2] = '\0';
+            if (s[i - 1] == '\n')
+                read_is_after_newline = 1;
+            else
+            {
+                read_is_after_newline = 0;
+                if (s[i - 1] == '\r')
+                    read_is_after_carriage_return = 1;
+                else
+                    read_is_after_carriage_return = 0;
+            }
         }
-        else if (l < max_length && buffer[l - 1] == '\n')
-        {
-            buffer[l - 1] = '\0';
-        }
-        // else assume the buffer doesn't end with any form of enter
-
-        printf(fmt, buffer);
     }
-
     return 0;
 }
