e0f5117d0ae052bbe7ad143e8176b67fb202f6a9
Tomás Raiti <46955459+Secreto31126@users.noreply.github.com>
2024-11-24 23:45:53 -0300

    Simplified bytestuffer

--- src/bytestuff/main.c
+++ src/bytestuff/main.c
@@ -1,54 +1,57 @@
-#include <math.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
-
-#define BLOCK 512
 
 /**
  * @brief Parse a file and make each line POP3 compliant
  */
 int main()
 {
-    char s[BLOCK];
-    int read_is_after_newline = 1, read_is_after_carriage_return = 0, last_print, i;
-    while (fgets(s, BLOCK, stdin) != NULL)
+    char input[512];
+    char prev = '\n';
+    while (fgets(input, sizeof(input), stdin))
     {
-        last_print = 0;
-        if (s[0] == '\n' && !read_is_after_carriage_return)
-            putchar('\r');
-        else if (s[0] == '.' && read_is_after_newline)
+        size_t len = strlen(input);
+
+        // Just in case something weird happens
+        if (!len)
+        {
+            break;
+        }
+
+        char actual = input[len - 1];
+
+        if (prev == '\r' && input[0] == '\n')
+        {
+            putchar('\n');
+            prev = actual;
+            continue;
+        }
+
+        if (prev == '\n' && input[0] == '.')
+        {
             putchar('.');
-        for (i = 1; s[i]; i++)
-        {
-            if (s[i] == '\n' && s[i - 1] != '\r')
-            {
-                fwrite(s + last_print, sizeof(char), i - last_print, stdout);
-                putchar('\r');
-                last_print = i;
-            }
-            else if (s[i] == '.' && s[i - 1] == '\n')
-            {
-                fwrite(s + last_print, sizeof(char), i - last_print + 1, stdout);
-                putchar('.');
-                last_print = i;
-            }
         }
-        if (i > last_print)
-            fwrite(s + last_print, sizeof(char), i - last_print, stdout);
-        if (i > 0)
+
+        if (actual != '\n')
         {
-            if (s[i - 1] == '\n')
-                read_is_after_newline = 1;
-            else
-            {
-                read_is_after_newline = 0;
-                if (s[i - 1] == '\r')
-                    read_is_after_carriage_return = 1;
-                else
-                    read_is_after_carriage_return = 0;
-            }
+            printf("%s", input);
+            prev = actual;
+            continue;
         }
+
+        input[len - 1] = '\0';
+        if (len != 1 && input[len - 2] == '\r')
+        {
+            input[len - 2] = '\0';
+        }
+
+        printf("%s\r\n", input);
+
+        // Only flush when we are sure we have a full line
+        fflush(stdout);
+        prev = actual;
     }
+
     return 0;
 }
