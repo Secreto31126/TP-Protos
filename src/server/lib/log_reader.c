5e2ae68923b25f4870521e3171ecf5a468ea80f4
LucasLonegro <llonegrogurfinkel@itba.edu.ar>
2024-11-26 14:03:28 -0300

    this method is getting very bloated and I can't think of a nice way to extract it

--- src/server/lib/log_reader.c
+++ src/server/lib/log_reader.c
@@ -25,6 +25,24 @@ char *parse_log(log l, data_parser parser)
     char *data_s;
     if (parser != NULL && l.data != NULL && (data_s = parser(l.data)) != NULL)
     {
+        if (!strcmp(l.ip, l.username))
+        {
+            char *__restrict__ __format = "%s: %s; %s. INFO: %s.";
+            uint64_t len = strlen(time_s) + strlen(type_s) + strlen(l.username) + sizeof(__format) - 4;
+            char *s = calloc(len, sizeof(char));
+            snprintf(s, len, __format, time_s, type_s, l.username, data_s);
+            free(data_s);
+            return s;
+        }
+        char *__restrict__ __format = "%s: %s; %s - %s. INFO: %s.";
+        uint64_t len = strlen(time_s) + strlen(type_s) + strlen(l.username) + strlen(l.ip) + sizeof(__format) - 5;
+        char *s = calloc(len, sizeof(char));
+        snprintf(s, len, __format, time_s, type_s, l.username, l.ip, data_s);
+        free(data_s);
+        return s;
+    }
+    if (!strcmp(l.ip, l.username))
+    {
         char *__restrict__ __format = "%s: %s; %s. INFO: %s.";
         uint64_t len = strlen(time_s) + strlen(type_s) + strlen(l.username) + sizeof(__format) - 4;
         char *s = calloc(len, sizeof(char));
@@ -32,9 +50,9 @@ char *parse_log(log l, data_parser parser)
         free(data_s);
         return s;
     }
-    char *__restrict__ __format = "%s: %s; %s.";
-    uint64_t len = strlen(time_s) + strlen(type_s) + strlen(l.username) + sizeof(__format) - 3;
+    char *__restrict__ __format = "%s: %s; %s - %s.";
+    uint64_t len = strlen(time_s) + strlen(type_s) + strlen(l.username) + sizeof(__format) - 4;
     char *s = calloc(len, sizeof(char));
-    snprintf(s, len, __format, time_s, type_s, l.username);
+    snprintf(s, len, __format, time_s, type_s, l.username, l.ip);
     return s;
 }
